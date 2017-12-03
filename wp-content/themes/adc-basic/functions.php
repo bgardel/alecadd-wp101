3b643b4f1d73b2332ef708c4a35d0c93f09e3b5e
Brian <bgardel@advdigitalconsulting.com>
2017-12-03 15:00:59 -0700

    commit basic file structure - wp101 part 4

---
+++ wp-content/themes/adc-basic/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function basictheme_script_enqueue() {
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/basic.css', array(), '1.0.0', 'all');
+    wp_enqueue_script('customjs', get_template_directory_uri() . '/js/basic.js', array(), '1.0.0', true);
+}
+
+add_action('wp_enqueue_scipts', 'basictheme_script_enqueue');
+
+function basictheme_theme_setup() {
+    add_theme_support('menus');
+    
+    register_nav_menu('primary', 'Primary Header Navigation');
+    register_nav_menu('secondary', 'Footer Navigation');
+}
+
+add_action('init', 'basictheme_theme_setup');
---
+++ wp-content/themes/adc-basic/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <?php wp_head(); ?>
+    </head>
+    
+    <?php 
+    if( is_front_page() ):
+        $basictheme_classes = array('basic-theme', 'homepage');
+    else:
+        $basictheme_classes = array('basic-theme', 'interiorpage');
+    endif;
+    ?>
+    
+    <body <?php body_class($basictheme_classes); ?>>
+        
+        <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
