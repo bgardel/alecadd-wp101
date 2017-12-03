2b2e8aba675611dc453ca74abaa545a8647bb497
Brian <bgardel@advdigitalconsulting.com>
2017-12-03 16:12:29 -0700

    added post types - wp 101 part 7

--- wp-content/themes/adc-basic/functions.php
+++ wp-content/themes/adc-basic/functions.php
@@ -19,3 +19,4 @@ add_action('init', 'basictheme_theme_setup');
 add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-thumbnails');
+add_theme_support('post-formats', array('aside','image','video'));
