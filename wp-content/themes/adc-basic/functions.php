8f4fe796f01661477bf990461ef3963afdb1ed04
Brian <bgardel@advdigitalconsulting.com>
2017-12-04 15:08:03 -0700

    added sidebar support - wp101 part 8

--- wp-content/themes/adc-basic/functions.php
+++ wp-content/themes/adc-basic/functions.php
@@ -64,10 +64,34 @@ add_theme_support('post-formats', array('aside','image','video'));
 
 /*
     ================================
-    remove WP Emoji
+    Remove WP Emoji code
     ================================
 */
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+
+/*
+    ================================
+    Sidebar function
+    ================================
+*/
+
+function basictheme_widget_setup() {    
+    register_sidebar( 
+        array(
+        	'name'          => 'Sidebar',
+        	'id'            => 'sidebar-1',    // ID should be LOWERCASE  ! ! !
+        	'description'   => 'Standard Sidebar',
+            'class'         => 'custom',
+        	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        	'after_widget'  => '</aside>',
+        	'before_title'  => '<h1 class="widget-title">',
+        	'after_title'   => '</h1>'
+        )
+    );
+}
+
+add_action('widgets_init','basictheme_widget_setup');
--- wp-content/themes/adc-basic/page-notitle.php
+++ wp-content/themes/adc-basic/page-notitle.php
@@ -6,14 +6,23 @@
 
 get_header(); ?>
 
-    <?php 
-    if( have_posts() ):
-        while ( have_posts() ): the_post(); ?>
+<div class="row">
+    <div class="col-xs-12 col-sm-8">
+        <?php 
+        if( have_posts() ):
+            while ( have_posts() ): the_post(); ?>
             
-            <p><?php the_content(); ?></p>
-        
-    <?php endwhile;
-    endif;
-    ?>
+                <?php get_template_part('content', get_post_format()); ?>
+            
+        <?php 
+            endwhile;
+        endif;
+        ?>
+    </div>
+    <div class="col-xs-12 col-sm-4">
+        <?php get_sidebar(); ?>
+    </div>
+
+</div>
 
 <?php get_footer(); ?>
